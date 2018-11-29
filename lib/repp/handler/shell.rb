d483afb1734e4f75e30ab5e5b09b35aad9dbbfe1
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2018-11-30 00:14:15 +0900

    Add ticker to shell handler

--- lib/repp/handler/shell.rb
+++ lib/repp/handler/shell.rb
@@ -5,7 +5,7 @@ module Repp
     class Shell
       module KeyboardHandler
         include EM::Protocols::LineText2
-        def initialize(app) @app = app.new; end
+        def initialize(app) @app = app; end
         def receive_line(data)
           reply_to = /@\w+/.match(data)&.[](1)
           message = Event::Receive.new(type: :message, body: data, reply_to: reply_to, bot?: false)
@@ -19,7 +19,15 @@ module Repp
       def self.run(app, options = {})
         yield self if block_given?
 
-        EM.run { EM.open_keyboard(KeyboardHandler, app) }
+        application = app.new
+        @ticker = Ticker.task(application) do |res|
+          if res.any?
+            $stdout.puts res.first
+          end
+        end
+
+        @ticker.run!
+        EM.run { EM.open_keyboard(KeyboardHandler, application) }
       end
 
       def self.stop
